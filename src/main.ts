b269eb03394f5cb65470b009409654f0f63c5f4d
simeondmnk <johan.simeon@karteldaun.com>
2026-03-17 00:16:58 +0700

    add vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { mount } from 'svelte'
+import { inject } from '@vercel/analytics'
 import './app.css'
 import App from './App.svelte'
+
+inject({ framework: 'svelte' })
 
 const app = mount(App, {
   target: document.getElementById('app')!,

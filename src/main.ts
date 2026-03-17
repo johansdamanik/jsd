92d0ba3509fd46975c2a061288040819f0dd092f
simeondmnk <johan.simeon@karteldaun.com>
2026-03-17 14:10:14 +0700

    feat: add speed insight

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { mount } from 'svelte'
 import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
+
 import './app.css'
 import App from './App.svelte'
 
+injectSpeedInsights({ framework: 'svelte' })
 inject({ framework: 'svelte' })
 
 const app = mount(App, {
